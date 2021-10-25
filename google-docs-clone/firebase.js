28e85dac05cacc401fde1f2c58a73f49dbd1edaf
MBusto <mikebusto4@gmail.com>
2021-10-25 16:18:20 -0400

    Setting up Firebase v8

---
+++ google-docs-clone/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbjO7NbQA_HDTo0hqUP9aQMSl9xlee_kE",
+    authDomain: "docs-clone-6f70e.firebaseapp.com",
+    projectId: "docs-clone-6f70e",
+    storageBucket: "docs-clone-6f70e.appspot.com",
+    messagingSenderId: "413521747041",
+    appId: "1:413521747041:web:2de025cc208b9ef7909e20"
+};
