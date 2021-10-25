73daccc02906d74705c1ba12cf3f6adea4e145fc
MBusto <mikebusto4@gmail.com>
2021-10-25 12:40:18 -0400

    feat: working on Header Component

--- google-docs-clone/components/Header.js
+++ google-docs-clone/components/Header.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
+import Button from "@material-tailwind/react/Button";
+import Icon from "@material-tailwind/react/Icon";
+
 function Header() {
     return (
-        <div>
-            <h1>hello from Header</h1>
+        <div className='sticky top-0 z-50 flex items-center px-4 py-2 shadow-lg bg-white'>
+            <Button
+                color='blue'
+                buttonType='outline'
+                rounded={true}
+                iconOnly={true}
+                ripple='dark'
+                className='h-20 w-20 border-0'
+            >
+                <Icon name='menu' size='3xl' />
+            </Button>
+
+            <Icon name='description' size='5xl' color='blue' />
+            <h1 className='ml-2 text-white text-2xl'>
+                Docs
+            </h1>
+
         </div>
     )
 }
