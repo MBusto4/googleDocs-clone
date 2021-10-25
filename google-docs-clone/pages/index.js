f6382e6cd83bc7492a901dc7ac4b8ccc65f7c99d
MBusto <mikebusto4@gmail.com>
2021-10-25 14:57:08 -0400

    feat: styled where the documents will go

--- google-docs-clone/pages/index.js
+++ google-docs-clone/pages/index.js
@@ -42,11 +42,12 @@ export default function Home() {
         </div>
       </section>
 
-      <section>
-        <div className=' max-w-3xl mx-auto py-8'>
-          <div>
-            <h2>My Documents</h2>
-            <p>Date Created</p>
+      <section className='bg-[#4e338a] px-10 md:px-0'>
+        <div className='max-w-3xl mx-auto py-8 text-sm'>
+          <div className='flex items-center justify-between pb-5 text-white'>
+            <h2 className='font-medium flex-grow'>My Documents</h2>
+            <p className='mr-10'>Date Created</p>
+            <Icon name='folder' size='3xl' color='white' />
           </div>
         </div>
       </section>
