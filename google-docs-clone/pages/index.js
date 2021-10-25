b38853c69d877ecce7359ad824ce2ced6743f2f9
MBusto <mikebusto4@gmail.com>
2021-10-25 12:05:18 -0400

    newComponent: Working on Header Component

--- google-docs-clone/pages/index.js
+++ google-docs-clone/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -7,6 +8,8 @@ export default function Home() {
         <title>GoogleDocs Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
 
     </div>
   )
