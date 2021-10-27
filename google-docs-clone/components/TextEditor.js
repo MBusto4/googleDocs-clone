938033103b98e12dfd1f1cf36b5ce994dc153219
MBusto <mikebusto4@gmail.com>
2021-10-27 02:10:41 -0400

    feat: text editor working and text saving

--- google-docs-clone/components/TextEditor.js
+++ google-docs-clone/components/TextEditor.js
@@ -1,11 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import dynamic from 'next/dynamic'
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-// import { Editor } from "react-draft-wysiwyg";
-import { convertToRaw, EditorState } from 'draft-js';
 import { db } from '../firebase';
 import { useSession } from 'next-auth/react'
 import { useRouter } from "next/dist/client/router"
+import Button from '@material-tailwind/react/Button'
+import Icon from '@material-tailwind/react/Icon'
+import {
+    useDocumentOnce
+} from "react-firebase-hooks/firestore"
+import { EditorState } from "draft-js";
+import { convertFromRaw, convertToRaw } from "draft-js";
+
+
 
 //server side doesnt have access to the window so we do this
 const Editor = dynamic(
@@ -22,6 +29,22 @@ function TextEditor() {
     const { data: sessionData } = useSession()
     const router = useRouter()
     const { id } = router.query
+
+    const [snapshot, loading] = useDocumentOnce(
+        db.collection('userDocs')
+            .doc(sessionData?.user?.email)
+            .collection('docs')
+            .doc(id))
+
+    useEffect(() => {
+        if (snapshot?.data()?.editorState) {
+            setEditorState(
+                EditorState.createWithContent(
+                    convertFromRaw(snapshot?.data()?.editorState)
+                )
+            );
+        }
+    }, [snapshot]);
 
     const onEditorStateChange = (editorState) => {
         setEditorState(editorState)
@@ -48,6 +71,23 @@ function TextEditor() {
                 editorClassName='mt-6 bg-white shadow-lg max-w-5xl mx-auto mb-12 border p-10'
 
             />
+            <div className='flex h-screen'>
+                <div className='mx-auto'>
+                    <Button
+                        onClick={() => router.push('/')}
+                        color='lightBlue'
+                        buttonType='filled'
+                        size='regular'
+                        className='h-10'
+                        rounded={false}
+                        block={false}
+                        iconOnly={false}
+                        ripple='light'
+                    >
+                        <Icon name='home' size='md' /> SAVE AND GO BACK HOME
+                    </Button>
+                </div>
+            </div>
         </div>
     )
 }
